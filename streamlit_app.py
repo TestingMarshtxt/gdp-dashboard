8049645cc59e28dda301387dc39fccebf9ababf0
TestingMarshtxt <mmorris04@txtav.com>
2024-07-19 16:05:27 +0000

    testing experimental user email

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,12 @@ from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
+    page_title=f"{st.secrets.testing.pass}",
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
-
 @st.cache_data
 def get_gdp_data():
     """Grab GDP data from a CSV file.
